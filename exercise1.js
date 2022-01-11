099df7eb0590525e4ccc776b69db39203538592d
Amoy N <moyanich@users.noreply.github.com>
2022-01-11 13:46:29 -0500

    10 Days of Javascript
    Complete Day 1

---
+++ exercise1.js
@@ -0,0 +1,28 @@
+/**
+ * 
+Exercise
+I now have a coding exercise for you to complete.
+
+I want you to edit this code such that the value of result is the average of the numerical values of a, b,c and d, rounded up to the nearest whole number. Note the variables are currently strings, but I want the average of the numerical values.
+
+Do this without editing the values that a, b, c and d are assigned.
+
+Next I want you to assign the value of a random die roll to randomDieRoll. A die of course has six sides, so the result should be created by generating a random number between 1 and 6, inclusive.
+
+The solution is shown in the next video lecture. 
+
+*/
+ 
+
+var a = '10';
+var b = '5';
+var c = '20';
+var d = '30';
+
+var result = Number(a) + Number(b) + Number(c) + Number(d);
+console.log('Exercise Answer: ' + Math.ceil(result/4));
+
+
+//var randomDieRollOutcome = Math.floor(Math.random() * 6);
+var randomDieRollOutcome = Math.floor(Math.random() * 6) + 1;
+console.log('Exercise Answer: ' + randomDieRollOutcome);
